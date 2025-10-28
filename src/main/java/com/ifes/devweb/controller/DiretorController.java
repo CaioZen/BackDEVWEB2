6b217418547dcd377fd6f47f1994e593857b5ac8
CaioZen <caiozengoncalves@gmail.com>
2025-10-28 14:15:01 -0300

    :memo: added Diretor documentation

--- src/main/java/com/ifes/devweb/controller/DiretorController.java
+++ src/main/java/com/ifes/devweb/controller/DiretorController.java
@@ -2,6 +2,8 @@ package com.ifes.devweb.controller;
 
 import com.ifes.devweb.model.Diretor;
 import com.ifes.devweb.service.DiretorService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +20,45 @@ public class DiretorController {
         this.diretorService = diretorService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Diretor cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar o diretor")
+    })
     @PostMapping("/inserir")
     public ResponseEntity<Diretor> inserirDiretor(@RequestBody Diretor diretor){
         return ResponseEntity.ok(diretorService.salvarDiretor(diretor));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listando diretores")
+    })
     @GetMapping("/listar")
     public ResponseEntity<List<Diretor>> listarDiretores(){
         return ResponseEntity.ok(diretorService.listarDiretores());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Diretor encontrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Diretor não encontrado")
+    })
     @GetMapping("/buscar/{id}")
     public ResponseEntity<Diretor> buscarDiretorPorId(@PathVariable String id){
         return ResponseEntity.ok(diretorService.buscarDiretorPorId(UUID.fromString(id)));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Diretor alterado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Diretor não encontrado")
+    })
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<Diretor> atualizarDiretor(@PathVariable String id, @RequestBody Diretor diretorAtualizado){
         return ResponseEntity.ok(diretorService.atualizarDiretor(UUID.fromString(id), diretorAtualizado));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Diretor excluido com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Diretor não encontrado")
+    })
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarDiretor(@PathVariable String id) {
         diretorService.deletarDiretor(UUID.fromString(id));
