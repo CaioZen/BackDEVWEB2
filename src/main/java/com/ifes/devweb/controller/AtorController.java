baf13d9c522a648445a56bef881216202c5380f5
CaioZen <caiozengoncalves@gmail.com>
2025-10-29 11:25:01 -0300

    :memo: added Ator documentation

--- src/main/java/com/ifes/devweb/controller/AtorController.java
+++ src/main/java/com/ifes/devweb/controller/AtorController.java
@@ -2,6 +2,8 @@ package com.ifes.devweb.controller;
 
 import com.ifes.devweb.model.Ator;
 import com.ifes.devweb.service.AtorService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,26 +19,45 @@ public class AtorController{
         this.atorService = atorService;
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ator cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cadastrar ator")
+    })
     @PostMapping("/inserir")
     public ResponseEntity<Ator> inserirAtor(@RequestBody Ator ator){
         return ResponseEntity.ok(atorService.salvarAtor(ator));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listando atores"),
+    })
     @GetMapping("/listar")
     public ResponseEntity<List<Ator>> listarAtores(){
         return ResponseEntity.ok(atorService.listarAtores());
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ator encontrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Ator não encontrado")
+    })
     @GetMapping("/buscar/{id}")
     public ResponseEntity<Ator> buscarAtorPorId(@PathVariable String id){
         return ResponseEntity.ok(atorService.buscarAtorPorId(UUID.fromString(id)));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ator alterado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Ator não encontrado")
+    })
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<Ator> atualizarAtor(@PathVariable String id, @RequestBody Ator atorAtualizado){
         return ResponseEntity.ok(atorService.atualizarAtor(UUID.fromString(id), atorAtualizado));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ator excluido com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Ator não encontrado")
+    })
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarAtor(@PathVariable String id) {
         atorService.deletarAtor(UUID.fromString(id));
